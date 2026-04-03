cef8ae832f46c3d3cca872ab1c864532adb797f7
MakailBrooks <78831037+MakailBrooks@users.noreply.github.com>
2026-04-03 14:59:45 -0500

    Update SensorDataProcessor.java

--- demo/src/main/java/com/sensordata/SensorDataProcessor.java
+++ demo/src/main/java/com/sensordata/SensorDataProcessor.java
@@ -37,6 +37,9 @@ public class SensorDataProcessor{
         BufferedWriter out;
 
         // Write racing stats data into a file
+
+
+        
         try {
             out = new BufferedWriter(new FileWriter("RacingStatsData.txt"));
 
@@ -44,28 +47,19 @@ public class SensorDataProcessor{
                 for (j = 0; j < data[0].length; j++) {
                     for (k = 0; k < data[0][0].length; k++) {
                         data2[i][j][k] = data[i][j][k] / d - Math.pow(limit[i][j], 2.0);
-
-                        if (average(data2[i][j]) > 10 && average(data2[i][j]) < 50)
+                        if ((average(data2[i][j]) > 10 && average(data2[i][j]) < 50) || (Math.max(data[i][j][k], data2[i][j][k]) > data[i][j][k]))
                             break;
-                        else if (Math.max(data[i][j][k], data2[i][j][k]) > data[i][j][k])
-                            break;
-                        else if (Math.pow(Math.abs(data[i][j][k]), 3) < Math.pow(Math.abs(data2[i][j][k]), 3)
-                                && average(data[i][j]) < data2[i][j][k] && (i + 1) * (j + 1) > 0)
+                        else if (Math.pow(Math.abs(data[i][j][k]), 3) < Math.pow(Math.abs(data2[i][j][k]), 3) && average(data[i][j]) < data2[i][j][k] && (i + 1) * (j + 1) > 0)
                             data2[i][j][k] *= 2;
                         else
                             continue;
                     }
-                }
-            }
-
-            for (i = 0; i < data2.length; i++) {
-                for (j = 0; j < data2[0].length; j++) {
                     out.write(data2[i][j] + "\t");
                 }
             }
 
-            out.close();
 
+            out.close();
             long endTime = System.nanoTime();
             long elapsedMs = (endTime - startTime) / 1_000_000;
             System.out.println("calculate() completed in " + elapsedMs + " ms");
